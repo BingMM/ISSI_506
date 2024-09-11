e8bdecc003be7eea882b27a260c88c1f342ae9e1
bingMM <michael.madelaire@uib.no>
2024-09-11 17:03:29 +0200

    Script for determining the cubedsphere grid is done. Folder >figures< added to store relevant figures. Folder >data< added to store relevant data. Pickled grid saved in data. Visualization of grid saved in figures.

--- step_1_define_grid.py
+++ step_1_define_grid.py
@@ -2,24 +2,112 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-
 from secsy import cubedsphere
+import lompe
+import gemini3d.read
+import dipole
+import polplot
+import pickle
 
+#%% Hyper parameters
+
+target_altitude = 110
+
+#%% Import GEMINI grid
+
+xg = gemini3d.read.grid('/home/bing/BCSS-DAG Dropbox/Michael Madelaire/work/temp_storage/hack_version/inputs/simgrid.h5')
+
+alt_id = np.argmin(abs(xg['alt'][:, 0, 0] - target_altitude*1e3))
 
 #%%
 
-ex
+fig = plt.figure()
+ax = plt.gca()
+pax = polplot.Polarplot(ax)
+pax.scatter(xg['glat'][alt_id, :, :].flatten(), xg['glon'][alt_id, :, :].flatten()/15)
 
-import sys
+#%% Quick visualization of GEMINI grid
 
-base = '/home/bing/BCSS-DAG Dropbox/Michael Madelaire/work/code/repos/'
-sys.path.append(base + 'd')
+plt.ioff()
 
+fig, axs = plt.subplots(1, 2, figsize=(15, 9))
+paxs = [polplot.Polarplot(axs[0]), polplot.Polarplot(axs[1])]
+paxs[0].scatter(xg['glat'][alt_id, :, :].flatten(),
+                xg['glon'][alt_id, :, :].flatten()/15)
+axs[0].set_title('GEMINI : Geo : {} km'.format(target_altitude))
 
-sys.path.append('~/BCSS-DAG Dropbox/Michael Madelaire/work/code/repos/lompe')
-sys.path.append('~/BCSS-DAG Dropbox/Michael Madelaire/work/code/repos/')
+paxs[1].plot(90-xg['theta'][alt_id, :, :].flatten()/np.pi*180,
+             xg['phi'][alt_id, :, :].flatten()/np.pi*180/15)
+axs[1].set_title('GEMINI : Mag : {} km'.format(target_altitude))
 
-import dipole
-from secsy import cubedsphere
-import lompe
+plt.savefig('/home/bing/BCSS-DAG Dropbox/Michael Madelaire/work/code/repos/ISSI_506/figures/GEMINI_grid.png', bbox_inches='tight')
+plt.close('all')
+plt.ion()
+
+#%% Define cubedsphere grid
+
+# Cubedsphere grid center
+glat_c, glon_c = 72, 352
+position = (glon_c, glat_c)
+
+# Orientation towards magnetic pole
+glat_pole, glon_pole = 79, 289 # From Jone's code who got it from Matt's code...
+orientation = np.degrees(lompe.data_tools.dataloader.getbearing(np.array([glat_c]), 
+                                                                np.array([glon_c]), 
+                                                                np.array([glat_pole]), 
+                                                                np.array([glon_pole])))
+
+# Other grid parameters
+L, W = 8000*1e3, 4000*1e3
+Lres, Wres = 10*1e3, 10*1e3
+RE = 6371.2*1e3
+RI = RE + 110*1e3
+
+# The grid
+grid = cubedsphere.CSgrid(cubedsphere.CSprojection(position, -orientation[0]),
+                          L, W, Lres, Wres, R=RI)
+
+#%% Is this dipole correct? Yes
+
+dip = dipole.Dipole(dipole_pole=(glat_pole, glon_pole))
+glat, glon = dip.mag2geo(90-xg['theta'][alt_id, :, :].flatten()/np.pi*180, 
+                         xg['phi'][alt_id, :, :].flatten()/np.pi*180)
+
+plt.ioff()
+
+plt.figure(figsize=(10, 10))
+ax = plt.gca()
+pax = polplot.Polarplot(ax)
+pax.scatter(xg['glat'][alt_id, :, :].flatten(),
+            xg['glon'][alt_id, :, :].flatten()/15, label='Geo')
+pax.scatter(glat, glon/15, alpha=.1, label='Mag->Geo')
+
+plt.title('GEMINI Mag -> Geo via Dipole @ {} km'.format(target_altitude))
+plt.legend()
+
+plt.savefig('/home/bing/BCSS-DAG Dropbox/Michael Madelaire/work/code/repos/ISSI_506/figures/GEMINI_grid_conversion_test.png', bbox_inches='tight')
+plt.close('all')
+plt.ion()
+
+#%% Quick visual comparison between the GEMINI and cubedsphere grid
+
+plt.ioff()
+
+fig = plt.figure(figsize=(10, 10))
+ax = plt.gca()
+pax = polplot.Polarplot(ax)
+
+pax.scatter(xg['glat'][alt_id, :, :].flatten(),
+            xg['glon'][alt_id, :, :].flatten()/15)
+pax.scatter(grid.lat_mesh.flatten(), grid.lon_mesh.flatten()/15, alpha=.01)
+pax.scatter(grid.projection.position[1], grid.projection.position[0]/15)
+
+plt.savefig('/home/bing/BCSS-DAG Dropbox/Michael Madelaire/work/code/repos/ISSI_506/figures/GEMINI_cubedsphere_grid_comparison.png', bbox_inches='tight')
+plt.close('all')
+plt.ion()
+
+#%% Save the grid
+
+with open('/home/bing/BCSS-DAG Dropbox/Michael Madelaire/work/code/repos/ISSI_506/data/cubedsphere_grid.pkl', 'wb') as f:
+    pickle.dump(grid, f)
 
