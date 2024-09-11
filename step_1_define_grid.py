be74a5af459a4a2839b2c5efb2b92a1f637f3a61
bingMM <michael.madelaire@uib.no>
2024-09-11 12:07:41 +0200

    Placeholder script for defining the cubedsphere grid used throughout the analysis.

---
+++ step_1_define_grid.py
@@ -0,0 +1,25 @@
+#%% Import
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from secsy import cubedsphere
+
+
+#%%
+
+ex
+
+import sys
+
+base = '/home/bing/BCSS-DAG Dropbox/Michael Madelaire/work/code/repos/'
+sys.path.append(base + 'd')
+
+
+sys.path.append('~/BCSS-DAG Dropbox/Michael Madelaire/work/code/repos/lompe')
+sys.path.append('~/BCSS-DAG Dropbox/Michael Madelaire/work/code/repos/')
+
+import dipole
+from secsy import cubedsphere
+import lompe
+
